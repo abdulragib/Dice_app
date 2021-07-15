6fc650541fabd199335c66bf3cc4f7f380d65732
Abdul Ragib <70109434+abdulragib@users.noreply.github.com>
2021-07-15 14:07:45 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.black,
         appBar: AppBar(
+          centerTitle: true,
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.white10,
         ),
         body: DicePage(),
       ),
@@ -15,9 +17,52 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber =1;
+  int rightDiceNumber =1;
+
+  void Dice(){
+    setState(() {
+      leftDiceNumber =Random().nextInt(6)+1;
+      rightDiceNumber =Random().nextInt(6)+1;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.black,
+              ),
+              onPressed: Dice,
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.black,
+                ),
+              onPressed: Dice,
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
+//nextInt(6) whatever we provide in parenthesis it reduce by 1 so dont
+// have 0 image so can add + 1 so it will update 0+1 =1 and we get 1 image
+// bydefault it will generate 0 to n-1 number
